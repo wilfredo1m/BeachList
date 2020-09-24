853d0158596a87dd48c282bdef8ac8f03d2662ed
Viet Nguyen <Viet1050@gmail.com>
2020-09-23 20:54:57 -0700

    Corrected variable names

--- app/src/main/java/com/example/beachlist/CreateAccount.java
+++ app/src/main/java/com/example/beachlist/CreateAccount.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
-    private Button createAccountButton, homeScreenButton;
+    private Button createAccountButton, cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +25,8 @@ public class CreateAccount extends AppCompatActivity {
         });
 
         // Cancel Button
-        createAccountButton = (Button) findViewById(R.id.btnCancel);
-        createAccountButton.setOnClickListener(new View.OnClickListener() {
+        cancelButton = (Button) findViewById(R.id.btnCancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openLoginScreen();
