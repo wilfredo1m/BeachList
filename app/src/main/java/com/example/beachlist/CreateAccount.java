2b955d50a87d7ff82832de63f3ea34bc238e8093
Viet Nguyen <Viet1050@gmail.com>
2020-09-24 14:59:23 -0700

    Linked profile picture button in create account page to phones camera roll

--- app/src/main/java/com/example/beachlist/CreateAccount.java
+++ app/src/main/java/com/example/beachlist/CreateAccount.java
@@ -4,26 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class CreateAccount extends AppCompatActivity {
     private Button createAccountButton, cancelButton;
     private EditText fName, lName, email, password, gradDate, phoneNum;
+    private ImageButton profilePic;
+    public static final int GET_FROM_GALLERY = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        // Set Profile Picture
+        profilePic = (ImageButton) findViewById(R.id.btnProfilePic);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Access the phones camera roll to let user pick their profile picture
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+            }
+        });
+
         // Create Account Button
         createAccountButton = (Button) findViewById(R.id.btnCreateAccount2);
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Input entered in each field
+                // Gets input entered in each field
                 fName = (EditText) findViewById(R.id.etFirstName);
                 lName = (EditText) findViewById(R.id.etLastName);
                 email = (EditText) findViewById(R.id.etEmail2);
