1d073a7d17621ec01cde58434bbda3912f85fd8d
Viet Nguyen <Viet1050@gmail.com>
2020-09-28 22:11:34 -0700

    Created and linked java files for the new pages

---
+++ app/src/main/java/com/example/beachlist/ToolbarNoSearchbar.java
@@ -0,0 +1,14 @@
+package com.example.beachlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToolbarNoSearchbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar_no_searchbar);
+    }
+}
