83604adfae09f664d8e238426e9ac656b162cee2
Viet Nguyen <Viet1050@gmail.com>
2020-10-04 17:45:05 -0700

    Renamed files to match correct pages / corrected buttons changed during testing

--- app/src/main/java/com/example/beachlist/HomeScreenAfterLogin.java
+++ app/src/main/java/com/example/beachlist/HomeScreenAfterLogin.java
@@ -21,41 +21,8 @@ public class HomeScreenAfterLogin extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchbar);
-     //   BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
-      //  bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationMethod);
-       // getSupportFragmentManager().beginTransaction().replace(R.id.fixing_this_fragment,new fragment1());
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
         NavController navController = Navigation.findNavController(this,  R.id.fixing_this_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
-
-/*
-    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationMethod=new
-            BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment fragment= null;
-                    switch(menuItem.getItemId())
-                    {
-                        case R.id.fragment1:
-                            fragment= new fragment1();
-                            break;
-                        case R.id.fragment2:
-                            fragment= new fragment2();
-                            break;
-                        case R.id.fragment3:
-                            fragment= new fragment3();
-                            break;
-                        case R.id.fragment4:
-                            fragment= new fragment4();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fixing_this_fragment,fragment).commit();
-                    return false;
-                }
-
-
-            };
-*/
-
 }
