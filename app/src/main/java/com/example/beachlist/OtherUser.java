2ba2b43ac1b1fbf60985f5c144a7d8a35ea36732
Bernardo Yusti <50193612+byusti@users.noreply.github.com>
2020-10-13 21:25:18 -0700

    implement OtherUser class

--- app/src/main/java/com/example/beachlist/OtherUser.java
+++ app/src/main/java/com/example/beachlist/OtherUser.java
@@ -1,6 +1,42 @@
 package com.example.beachlist;
 
 public class OtherUser {
-    String userid;
-    String name;
+    String firstName;
+    String lastName;
+    String imageUrl;
+
+    public OtherUser() {
+        //default initialzer
+        //required by firebase for converting the object to json
+    }
+
+    public OtherUser(String firstName, String lastName, String imageUrl) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
