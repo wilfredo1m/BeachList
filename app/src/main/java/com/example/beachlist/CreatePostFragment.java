2c94e9dc6ee680ee1493701b47e9f655b2adb874
wil <wilfredo.mendivil@student.csulb.edu>
2020-10-14 14:53:45 -0700

    -starting create posting page -adding a few new pages

--- app/src/main/java/com/example/beachlist/CreatePostFragment.java
+++ app/src/main/java/com/example/beachlist/CreatePostFragment.java
@@ -8,19 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.beachlist.databinding.ActivityMainBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CreatePostFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CreatePostFragment extends Fragment {
+    ActivityMainBinding binding;
+    String[] descriptionData= {"Select Photo","Item Description","Confirm"};
+    int current_state = 0;
+
     public CreatePostFragment() {
         // Required empty public constructor
     }
+    //TODO add the screen efffect of the select picture from gallery or from camera to go to next page by bottom to up effect
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        //when u do a return it makes me question wehre teh fuck is the view here haha (3:42 in the vid)
+        //setContentView(binding.getRoot());
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_create_post, container, false);
     }
 }
