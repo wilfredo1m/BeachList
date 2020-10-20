6a9e8649ce519b855644b1a642c1401704b51309
Bernardo Yusti <50193612+byusti@users.noreply.github.com>
2020-10-20 13:37:49 -0700

    implement accepting and denying pending friends

--- app/src/main/java/com/example/beachlist/SelectedPendingFriend.java
+++ app/src/main/java/com/example/beachlist/SelectedPendingFriend.java
@@ -2,29 +2,44 @@ package com.example.beachlist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SelectedPendingFriend extends AppCompatActivity {
+    private static final String TAG = "error";
     ImageView profilePic;
     TextView firstName, lastName;
+    FirebaseDatabase database;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_pending_friend);
-
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
         profilePic = findViewById(R.id.iv_selected_pending_friend_image);
         firstName = findViewById(R.id.tv_pending_first_name);
         lastName = findViewById(R.id.tv_pending_last_name);
 
         // gets the pic and name of the user to display
-        int position = getIntent().getIntExtra("position",1);
+        final int position = getIntent().getIntExtra("position",1);
 
         // Sets the persons info in the correct fields to be displayed
         profilePic.setImageResource(PendingFriendsListTab.list.get(position).getImageProfile());
@@ -45,8 +60,33 @@ public class SelectedPendingFriend extends AppCompatActivity {
         acceptRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Temporarily sending to friends list to make sure it works
-                openFriendsListScreen();
+                DatabaseReference newFriendReference = database.getReference().child("users").child(user.getUid()).child("friends").child(PendingFriendsListTab.list.get(position).userId);
+                newFriendReference.setValue(new OtherUser(PendingFriendsListTab.list.get(position).getFirstName(), PendingFriendsListTab.list.get(position).getLastName(), "https://firebasestorage.googleapis.com/v0/b/beachlist-26c5b.appspot.com/o/images%2F1595294896?alt=media&token=c341b259-f2a5-45ad-97e1-04b770734db1"))
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                DatabaseReference deletePendingReference = database.getReference().child("users").child(user.getUid()).child("pending").child(PendingFriendsListTab.list.get(position).userId);
+                                deletePendingReference.removeValue()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                openFriendsListScreen();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "could not delete value");
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "could not add value");
+                            }
+                        });
             }
         });
 
@@ -54,8 +94,20 @@ public class SelectedPendingFriend extends AppCompatActivity {
         rejectRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Temporarily sending to pending friends list to make sure it works
-                openPendingFriendListScreen();
+                DatabaseReference deletePendingReference = database.getReference().child("users").child(user.getUid()).child("pending").child(PendingFriendsListTab.list.get(position).userId);
+                deletePendingReference.removeValue()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                openFriendsListScreen();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "could not delete value");
+                            }
+                        });
             }
         });
     }
