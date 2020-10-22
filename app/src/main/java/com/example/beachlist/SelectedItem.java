ee5ebf721dbe2a267d7b85944b482160998bd668
wil <wilfredo.mendivil@student.csulb.edu>
2020-10-21 21:19:50 -0700

    -created the selected item page -formatted the page -gave functionality to the back button for selected item and selected user

--- app/src/main/java/com/example/beachlist/SelectedItem.java
+++ app/src/main/java/com/example/beachlist/SelectedItem.java
@@ -1,6 +1,9 @@
 package com.example.beachlist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,19 @@ public class SelectedItem extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_other_user_item);
+
+    // Go back to User search list (temporarily going back to home)
+    Button backButton = findViewById(R.id.btn_back_from_user_item_page);
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+        @Override
+        public void onClick(View view) {
+            openHomeScreen();
+            }
+        });
+    }
+    public void openHomeScreen(){
+        Intent openScreen = new Intent(this, HomeScreenAfterLogin.class);
+        startActivity(openScreen);
     }
 }
