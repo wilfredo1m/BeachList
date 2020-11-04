b72d4f85041e253aef039e5d1447b910ffc8ed02
wil <wilfredo.mendivil@student.csulb.edu>
2020-11-04 14:20:50 -0800

    -added pages to hold recyclers for listings in users

---
+++ app/src/main/java/com/example/beachlist/ItemsInUser.java
@@ -0,0 +1,25 @@
+package com.example.beachlist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ItemsInUser extends Fragment {
+
+
+    public ItemsInUser() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_items_in_user, container, false);
+    }
+}
