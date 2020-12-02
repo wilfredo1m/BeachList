652ed02e3d2a976c359aa882c8220ee85534d9e4
randallt21 <randallt1211@gmail.com>
2020-12-02 10:02:00 -0800

    Removed unused variables from SelectedItem

--- app/src/main/java/com/example/beachlist/SelectedItem.java
+++ app/src/main/java/com/example/beachlist/SelectedItem.java
@@ -37,7 +37,7 @@ import java.util.HashMap;
 
 public class SelectedItem extends AppCompatActivity {
     private static final String TAG = "EXCEPTION";
-    ViewPager2 viewPager,reportedPager;
+    ViewPager2 viewPager;
     private final ArrayList<String> itemImages = new ArrayList<>();
     private FirebaseDatabase firebaseDatabase;
     private ListingData selectedListing;
@@ -46,9 +46,9 @@ public class SelectedItem extends AppCompatActivity {
     ArrayList<String>friendNameArray = new ArrayList<>();
     ArrayList<String>friendIDArray = new ArrayList<>();
 
-    ImageAdapterString adapter,adapter2;
+    ImageAdapterString adapter;
     ImageView userPicture;
-    TextView itemTitle, itemDescription, itemPrice, itemCategory, itemSellerFirstName, itemSellerLastName, reportedItemTitle, itemSellerEmail;
+    TextView itemTitle, itemDescription, itemPrice, itemCategory, itemSellerFirstName, itemSellerLastName, itemSellerEmail;
     ConstraintLayout itemPopUpWindow, mainItemWindow;
     Button reportItem, cancelReport,backButton,contactSeller,confirmReport,shareItemBtn;
     Spinner reportItemSpinner;
@@ -246,7 +246,7 @@ public class SelectedItem extends AppCompatActivity {
         confirmShareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String selectedFriend = shareItemSpinner.getSelectedItem().toString();
+                //String selectedFriend = shareItemSpinner.getSelectedItem().toString();
                 String friendID = friendIDArray.get(friendPositionIntValue);
                 String comment = commentForShareScreen.getText().toString();
                 sendToFriend(friendID, comment);
